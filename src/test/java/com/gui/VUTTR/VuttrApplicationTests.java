a54484b2268c422d1bf31d3c076e145a3a436881
Guilherme Vale <guilherme.vale98@gmail.com>
2024-03-17 22:29:12 -0300

    first commit

---
+++ src/test/java/com/gui/VUTTR/VuttrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.VUTTR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuttrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
